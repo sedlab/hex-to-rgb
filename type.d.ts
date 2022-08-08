c629c4339dc2f800f831fc5e32bd36d8623f6269
Дмитрий Сединкин <seadinkin2002@yandex.ru>
2022-08-08 16:30:26 +0500

    v1.0.0

---
+++ type.d.ts
@@ -0,0 +1,6 @@
+export declare type TFormatValue = string;
+export declare type TFormat = number;
+export declare type TAlphaValue = string;
+export declare type TAlpha = string | undefined;
+export declare type THexToRgbValue = string;
+export declare type THexToRgb = string | undefined;
